932efb3e2d04a19af58d5a124cafd2dcb979b8ca
Sigalsha <sigal.shalit.manor@gmail.com>
2022-04-28 17:24:48 +0300

    finish basic fetching images to carousel, make next and prev btn work

--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Grid } from "react-loader-spinner";
 import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import { fetchImageUrls } from "../api/index";
+import { fetchImages, fetchImage } from "../api/index";
 
 const Loader = styled.div`
   margin: 0 auto;
@@ -13,51 +13,125 @@ const Loader = styled.div`
 
 const Image = styled.img`
   object-fit: contain;
-  max-width: 90%;
+  max-width: 80%;
   height: 100%;
-  margin: auto 5%;
+  margin: 0 5%;
+
+  @media (max-width: 480px) {
+    max-width: 75%;
+  }
 `;
 
 const ImageCarousel = (props) => {
   const [loading, setLoading] = useState(true);
   const [fetchedImages, setFetchedImages] = useState([]);
-  // TODO - const [errorMsg, setError] = useState(false);
+  const [imagesLength, setImagesLength] = useState(0);
+  const [currentImgIndex, setCurrentImgIndex] = useState(0);
+
+  const [errorMsg, setError] = useState("");
 
   useEffect(() => {
-    console.log("useEffect");
+    console.log("useEffect for fetching all images");
+
     const fetchData = async () => {
-      const fetchedImages = await fetchImageUrls();
-      setFetchedImages(fetchedImages);
-      setLoading(false);
+      setLoading(true);
+      try {
+        const fetchedImages = await fetchImages();
+        console.log(fetchedImages);
+        setFetchedImages(fetchedImages);
+        setImagesLength(fetchedImages.length);
+        setLoading(false);
+      } catch (error) {
+        console.log("error: ", error);
+        setError(error);
+        setLoading(false);
+      }
     };
     fetchData();
   }, []);
 
+  useEffect(async () => {
+    setLoading(true);
+    console.log("useEffect for fetching single image");
+    try {
+      console.log(currentImgIndex);
+
+      await fetchImage(currentImgIndex);
+      console.log(currentImgIndex);
+
+      setLoading(false);
+    } catch (error) {
+      console.log("error: ", error);
+      setError(error);
+      setLoading(false);
+    }
+  }, [currentImgIndex]);
+
+  const handleNextClick = () => {
+    console.log(imagesLength);
+    setCurrentImgIndex(
+      currentImgIndex === imagesLength - 1 ? 0 : currentImgIndex + 1
+    );
+  };
+
+  const handlePrevClick = () => {
+    console.log(imagesLength);
+    console.log(currentImgIndex);
+
+    setCurrentImgIndex(
+      currentImgIndex === 0 ? imagesLength - 1 : currentImgIndex - 1
+    );
+  };
+
+  const loader = (
+    <Loader>
+      <Grid height="200" width="200" color="grey" ariaLabel="loading" />
+    </Loader>
+  );
+
+  const error = (
+    <Alert severity="error">
+      {errorMsg ? errorMsg : "An error occurred, please try again"}
+    </Alert>
+  );
+
+  const carousel = (
+    <>
+      <ArrowBackIosIcon sx={{ margin: "0 auto" }} onClick={handlePrevClick} />
+
+      {fetchedImages.length &&
+        fetchedImages.map((fetchedImage, index) => {
+          return (
+            <div key={index}>
+              {index === currentImgIndex && fetchedImage && (
+                <Image
+                  key={index}
+                  src={fetchedImages[currentImgIndex]}
+                  alt="image"
+                />
+              )}
+            </div>
+          );
+        })}
+
+      <ArrowForwardIosIcon
+        sx={{ margin: "0 auto" }}
+        onClick={handleNextClick}
+      />
+    </>
+  );
+
   return (
     <Container
       maxWidth="lg"
-      sx={{ height: "100vh", display: "flex", alignItems: "center" }}
+      sx={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
     >
-      {loading || !fetchImageUrls ? (
-        <Loader>
-          <Grid height="200" width="200" color="grey" ariaLabel="loading" />
-        </Loader>
-      ) : (
-        <>
-          <ArrowBackIosIcon />
-          <Box
-            sx={{
-              bgcolor: "#cfe8fc",
-              height: "80vh",
-              width: "50%",
-              margin: "0 auto"
-            }}
-          >
-            <Image src={fetchedImages[0]} alt="image" />
-          </Box>
-          <ArrowForwardIosIcon />
-        </>
-      )}
+      {loading ? loader : !fetchedImages || errorMsg ? error : carousel}
     </Container>
   );
 };
