026c7e62291975010dceb76282bfb7d4113daab3
Sigalsha <sigal.shalit.manor@gmail.com>
2022-04-28 15:41:23 +0300

    first commit. basic ui, WIP

--- src/task1/App.js
+++ src/task1/App.js
@@ -1,12 +1,14 @@
-import logo from '../logo.svg';
-import './App.css';
+import logo from "../logo.svg";
+import CssBaseline from "@mui/material/CssBaseline";
+import "./App.css";
 import ImageCarousel from "./ImageCarousel";
 
 function App() {
   return (
-      <div>
-        <ImageCarousel />
-      </div>
+    <div>
+      <CssBaseline />
+      <ImageCarousel />
+    </div>
   );
 }
 
--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Grid } from "react-loader-spinner";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import { fetchImageUrls } from "../api/index";
 
+const Loader = styled.div`
+  margin: 0 auto;
+`;
+
+const Image = styled.img`
+  object-fit: contain;
+  max-width: 90%;
+  height: 100%;
+  margin: auto 5%;
+`;
+
 const ImageCarousel = (props) => {
-    return <h1>Your code goes here</h1>;
+  const [loading, setLoading] = useState(true);
+  const [fetchedImages, setFetchedImages] = useState([]);
+  // TODO - const [errorMsg, setError] = useState(false);
+
+  useEffect(() => {
+    console.log("useEffect");
+    const fetchData = async () => {
+      const fetchedImages = await fetchImageUrls();
+      setFetchedImages(fetchedImages);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <Container
+      maxWidth="lg"
+      sx={{ height: "100vh", display: "flex", alignItems: "center" }}
+    >
+      {loading || !fetchImageUrls ? (
+        <Loader>
+          <Grid height="200" width="200" color="grey" ariaLabel="loading" />
+        </Loader>
+      ) : (
+        <>
+          <ArrowBackIosIcon />
+          <Box
+            sx={{
+              bgcolor: "#cfe8fc",
+              height: "80vh",
+              width: "50%",
+              margin: "0 auto"
+            }}
+          >
+            <Image src={fetchedImages[0]} alt="image" />
+          </Box>
+          <ArrowForwardIosIcon />
+        </>
+      )}
+    </Container>
+  );
 };
 export default ImageCarousel;
